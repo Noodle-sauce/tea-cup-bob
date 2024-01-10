b308f6eceb970432c836b3925e373c986ff0c331
Ajdin Omeragic <ajdin.omeragic@stu.ibu.edu.ba>
2024-01-10 20:01:19 +0100

    first commit

--- contracts/chai.sol
+++ contracts/chai.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.7;
 
 contract chai {
-    address payable owner;
+    address payable public owner;
 
     struct Memo {
         string name;
@@ -13,6 +13,14 @@ contract chai {
 
     Memo[] public memos;
 
+    event MemoAdded(string indexed name, address indexed from, uint timestamp);
+    event FundsWithdrawn(address indexed owner, uint amount);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function");
+        _;
+    }
+
     constructor() {
         owner = payable(msg.sender);
     }
@@ -21,12 +29,22 @@ contract chai {
         string calldata name,
         string calldata message
     ) external payable {
-        require(msg.value > 0, "Insuficiant funds!");
+        require(msg.value > 0, "Insufficient funds sent with the transaction");
         owner.transfer(msg.value);
         memos.push(Memo(name, message, block.timestamp, msg.sender));
+        emit MemoAdded(name, msg.sender, block.timestamp);
     }
 
     function getMemos() public view returns (Memo[] memory) {
         return memos;
     }
+
+    // function withdrawAllFunds() external onlyOwner {
+    //     uint contractBalance = address(this).balance;
+
+    //     require(contractBalance > 0, "No funds to withdraw");
+
+    //     owner.transfer(contractBalance);
+    //     emit FundsWithdrawn(owner, contractBalance);
+    // }
 }
